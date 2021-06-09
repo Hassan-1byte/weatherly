f26e0bd9c6fb36264c4cd0f8bdcb97c036346c42
Hassan Qayyum <sh.hassan.qayyum@gmail.com>
2021-06-10 02:17:57 +0500

    JS code re-edited.
    Signed-off-by: Hassan Qayyum <sh.hassan.qayyum@gmail.com>

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,62 @@
-const cityName = document.getElementById("cityName");
-const submitBtn = document.getElementById("btn");
-const city_name = document.getElementById("city_name");
-const temp_real_val = document.getElementById("temp_real_val");
-const temp_status = document.getElementById("temp_status");
+const cityName = document.getElementById('cityName');
+const submitBtn = document.getElementById('submitBtn');
 
-const datahide = document.querySelector(".middle_layer");
+const city_name = document.getElementById('city_name');
+const temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+const datahide = document.querySelector('.middle_layer');
 
-const getinfo = async (event) => {
-  event.preventDefault();
-  let cityVal = cityName.Value;
-  if (cityVal === "") {
-    city_name.innerText = `Please write the name before search`;
-    datahide.classList.add("data_hide");
-  } else {
-    try {
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=748b1cd6044bba318dd0f13b23074c78`;
-      const responce = await fetch(url);
-      const data = await responce.json();
-      const arrData = [data];
-      city_name.innerText = `${arrData[0].name} , ${arrData[0].sys.country}`;
-      temp_real_val.innerText = arrData[0].main.temp_real_val;
-      const tempMood = arrData[0].weather[0].main;
-      // Condition to check sunny or cloudy
-      if (tempMood === "Clear") {
-        temp_status.innerHTML =
-          "<i class='fas fa-sun' style='color: #eccc68;'></i>";
-      } else if (tempMood === "Clouds") {
-        temp_status.innerHTML =
-          "<i class='fas fa-cloud' style='color: #f1f2f6;'></i>";
-      } else if (tempMood === "Rain") {
-        temp_status.innerHTML =
-          "<i class='fas fa-cloud-rain' style='color: #a4b0be;'></i>";
-      } else {
-        temp_status.innerHTML =
-          "<i class='fas fa-sun' style='color: #eccc68;'></i>";
-      }
-      datahide.classList.remove("data_hide");
-    } catch {
-      city_name.innerText = `Please Enter The City Name Properly.`;
-      datahide.classList.add("data_hide");
+const getInfo = async(event) => {
+    event.preventDefault();
+
+    let cityVal = cityName.value;
+
+    if(cityVal === ""){
+        city_name.innerText = `Plz write the name before search`;
+        datahide.classList.add("data_hide");
+    }else{
+
+        try{
+           
+
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=b14425a6554d189a2d7dc18a8e7d7263`
+            const response = await fetch(url);
+
+            const data = await response.json();
+            const arrData = [data];
+
+            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+            temp_real_val.innerText = arrData[0].main.temp;
+            const tempMood = arrData[0].weather[0].main;
+            console.log(tempMood);
+
+            //condition to check sunny or cloudy
+            if (tempMood == "Clear") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
+            } else if (tempMood == "Clouds") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>";
+            } else if (tempMood == "Rain") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
+            } else {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud' style='color:#f1f2f6;'></i>";
+
+            }
+            datahide.classList.remove('data_hide');
+            cityVal = "";
+           
+       
+        }catch{
+            cityVal = " ";
+            datahide.classList.add("data_hide");
+            city_name.innerText =  `please enter the proper city name`;
+            console.log('please add the proper city name');
+        }
+        
     }
-  }
-};
-submitBtn.addEventListener("click", getinfo);
+}
+
+submitBtn.addEventListener('click', getInfo);
